9d44c2bd4a6cba570a2ea4d05a189206797571b1
hajar489 <hajar@theselectionlab.com>
2025-07-03 10:42:08 +0200

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 echo "▶️  Run update_linkedin_likes.py..."
 python update_linkedin_likes.py
 
 echo "📂  Voeg nieuwe CSV toe aan Git..."
 git add history/linkedin_likes_history_*.csv
+git add -A  # Voeg ook eventuele verwijderde/bijgewerkte bestanden toe
 
 echo "📝  Commit wijzigingen..."
 git commit -m "Voeg nieuwe history-bestanden toe"
